65cd1e6731d25e687b427ba89f45e3171144c531
Patrick Hanson <patrickhanson08@gmail.com>
2018-08-02 13:29:30 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Hello, <Text color="blue" value="Kenzie" /></h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+class Text extends React.Component {
+  render() {
+    return  <span style={{color: this.props.color}}>{this.props.value}</span>
+  }
+}
+
+export default App;
